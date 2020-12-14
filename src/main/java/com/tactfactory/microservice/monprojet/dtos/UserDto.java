f9dd3508954499951c92593127041c7067c39822
Antoine Cronier <antoine.cronier@tactfactory.com>
2020-12-14 12:00:17 +0100

    p1

---
+++ src/main/java/com/tactfactory/microservice/monprojet/dtos/UserDto.java
@@ -0,0 +1,29 @@
+package com.tactfactory.microservice.monprojet.dtos;
+
+public class UserDto {
+
+	private String firstname;
+	private String lastname;
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	@Override
+	public String toString() {
+		return "UserDto [firstname=" + firstname + ", lastname=" + lastname + "]";
+	}
+
+}
